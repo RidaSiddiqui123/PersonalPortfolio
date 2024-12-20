375254d65a745d1573f01a57e9090eba4321e9bf
ridasiddiqui123 <ridasiddiqui123@gmail.com>
2024-12-19 19:38:50 -0800

    Added email validation

--- backend/contact.php
+++ backend/contact.php
@@ -55,50 +55,56 @@ error_reporting(E_ALL);
 
     if ($first_name && $last_name && $email && $message) {
         
-        // 'C:/MAMP/htdocs/personalPortfolio/composer/vendor/autoload.php'
-        require './vendor/autoload.php';
+        if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+            // 'C:/MAMP/htdocs/personalPortfolio/composer/vendor/autoload.php'
+            require './vendor/autoload.php';
 
-        $mail = new PHPMailer(true);
+            $mail = new PHPMailer(true);
 
-        try {
-            //Server settings
-            $mail->SMTPDebug = 3;                      //Enable verbose debug output
-            $mail->isSMTP();                                            //Send using SMTP
-            $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
-            $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'ridasiddiqui123@gmail.com';                     //SMTP username
-            //ReactPortfolio
-            $mail->Password   = 'erdmahdsfynawuqh';                               //SMTP password
-            $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
-            $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
-        
-            //Recipients
-        
-            $mail->setFrom($email, 'Mailer');
-            $mail->addAddress('ridasiddiqui123@gmail.com', 'Contact Form');     //Add a recipient
-            // $mail->addAddress('ellen@example.com');               //Name is optional
-            $mail->addReplyTo($email);
-            // $mail->addCC('cc@example.com');
-            // $mail->addBCC('bcc@example.com');
-        
-            //Attachments
-            // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
-            // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
-        
-            //Content
-            $mail->isHTML(true);                                  //Set email format to HTML
-            $mail->Subject = 'Portfolio - Contact Form - ' . $email;
-            $mail->Body    = 'First Name: ' . $first_name . '<br />
-                                Last Name: ' . $last_name . '<br />
-                                Email: ' . $email . '<br />
-                                <br />
-                                Message: <br />' . $message;
-            // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-        
-            $mail->send();
-            echo 'Message has been sent';
-        } catch (Exception $e) {
-            echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            try {
+                //Server settings
+                $mail->SMTPDebug = 3;                      //Enable verbose debug output
+                $mail->isSMTP();                                            //Send using SMTP
+                $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+                $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+                $mail->Username   = 'ridasiddiqui123@gmail.com';                     //SMTP username
+                //ReactPortfolio
+                $mail->Password   = 'erdmahdsfynawuqh';                               //SMTP password
+                $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+                $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
+            
+                //Recipients
+            
+                $mail->setFrom($email, 'Mailer');
+                $mail->addAddress('ridasiddiqui123@gmail.com', 'Contact Form');     //Add a recipient
+                // $mail->addAddress('ellen@example.com');               //Name is optional
+                $mail->addReplyTo($email);
+                // $mail->addCC('cc@example.com');
+                // $mail->addBCC('bcc@example.com');
+            
+                //Attachments
+                // $mail->addAttachment('/var/tmp/file.tar.gz');         //Add attachments
+                // $mail->addAttachment('/tmp/image.jpg', 'new.jpg');    //Optional name
+            
+                //Content
+                $mail->isHTML(true);                                  //Set email format to HTML
+                $mail->Subject = 'Portfolio - Contact Form - ' . $email;
+                $mail->Body    = 'First Name: ' . $first_name . '<br />
+                                    Last Name: ' . $last_name . '<br />
+                                    Email: ' . $email . '<br />
+                                    <br />
+                                    Message: <br />' . $message;
+                // $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+            
+                $mail->send();
+                echo 'Message has been sent';
+            } catch (Exception $e) {
+                echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+            }
+        }
+
+        else {
+            echo "Invalid email!";
         }
     }
 
