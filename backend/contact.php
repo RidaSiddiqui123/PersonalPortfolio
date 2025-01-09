c646dd53925adc39714b8121977f7c72c9461b7c
ridasiddiqui123 <ridasiddiqui123@gmail.com>
2025-01-09 15:12:50 -0800

    Added env variables

--- backend/contact.php
+++ backend/contact.php
@@ -41,6 +41,8 @@ use PHPMailer\PHPMailer\Exception;
     $email = isset($_POST['email']) ? $_POST['email'] : '';
     $message = isset($_POST['message']) ? nl2br($_POST['message']) : '';
     
+    $username = getenv('EMAIL_USERNAME');
+    $password = getenv('EMAIL_PASSWORD');
 
     if ($first_name && $last_name && $email && $message) {
         
@@ -55,9 +57,9 @@ use PHPMailer\PHPMailer\Exception;
             $mail->isSMTP();                                            //Send using SMTP
             $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
             $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-            $mail->Username   = 'ridasiddiqui123@gmail.com';                     //SMTP username
+            $mail->Username   = $username;                     //SMTP username
             //ReactPortfolio
-            $mail->Password   = 'erdmahdsfynawuqh';                               //SMTP password
+            $mail->Password   = $password;                               //SMTP password
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
             $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
         
